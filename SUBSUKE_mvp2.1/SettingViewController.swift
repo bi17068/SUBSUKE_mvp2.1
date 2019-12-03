3d355f04d916605466c1e4730616ba40e1b7c67e
わかちこ。 <wakachiko0623@gmail.com>
2019-12-03 14:38:11 +0900

    Initial Commit

---
+++ SUBSUKE_mvp2.1/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  SUBSUKE_mvp2.1
+//
+//  Created by 若月祐樹 on 2019/12/02.
+//  Copyright © 2019 若月祐樹. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+    @IBOutlet weak var inputName: UITextField!
+    
+}
---
+++ SUBSUKE_mvp2.1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SUBSUKE_mvp2.1
+//
+//  Created by 若月祐樹 on 2019/11/29.
+//  Copyright © 2019 若月祐樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func settingButtonActioin(_ sender: Any) {
+        
+    }
+    
+}
+
